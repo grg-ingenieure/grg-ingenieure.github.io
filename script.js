702e50ee23cefabd54dd8180bb120636d356ec90
mariavkou <mariavkourouni@gmail.com>
2017-10-02 13:13:13 +0000

    Smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* Scrolling smooth between href attributes:
+ * http://jsfiddle.net/9SDLw/ */
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href')  ).offset().top
+    }, 1500);
+    return false;
+});
 /* single-page
  * var cards = ['home', 'about', 'leistungen', 'projekte', 'kontakt']
 function chooseCard(cardId) {
