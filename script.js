06ba5a76474262b311b81b9e2ec9162b4fc366fa
mariavkou <mariavkourouni@gmail.com>
2017-10-30 18:38:15 +0000

    Scrolling to the top of each href-id

--- script.js
+++ script.js
@@ -2,26 +2,8 @@
  * http://jsfiddle.net/9SDLw/ */
 $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href')  ).offset().top
+        scrollTop: $( $(this).attr('href')  ).offset().top - 50
     }, 1500);
     return false;
 });
-/* single-page
- * var cards = ['home', 'about', 'leistungen', 'projekte', 'kontakt']
-function chooseCard(cardId) {
-    var theCard = document.getElementById(cardId).getAttribute("href");
-    var sectionId = theCard.substring(1);
-    console.log(sectionId);
-    for (var i in cards){
-        if (cards[i] == sectionId) {
-            document.getElementById(cards[i]).style.display = "block";
-        } else {
-            document.getElementById(cards[i]).style.display = "none";
-        }
-    }
-    //document.getElementById(cardId).href = '#top';
-}*/
-
-
-
 
